d7fcaf17d6a456174570c5a34fde073e29d09e77
ShaunA-R <shaunreyes209@gmail.com>
2022-10-17 14:29:59 -0700

    FirstPush

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Shaun Aguirre-Reyes
+//10/17/22
+//2:21pm
+
+Console.Clear();
+
+string playAgain = "yes";
+while(playAgain == "yes"){
+
+Console.WriteLine("What is Your Name?");
+
+string myName = Console.ReadLine();
+
+Console.WriteLine("Good Afternoon " + myName);
+
+Console.WriteLine("Do you want to play again?");
+playAgain = Console.ReadLine();
+if(playAgain == "yes"){
+    Console.WriteLine("Lets play again");
+}else{
+    Console.WriteLine("Have a great day :)");
+}
+
+}
